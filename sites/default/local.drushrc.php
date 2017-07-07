69d6fc2cf139be781cb610cfce51ff820858451e
josephbayes <joseph.bayes@modea.com>
2017-07-07 16:56:47 -0400

    initial commit

---
+++ sites/default/local.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Local drush configuration for this project.
+ * 
+ * This will be copied to local.drushrc.php by BLT when `blt setup`
+ * or `blt local:setup` is run. Phing placeholders, like
+ * http://local.jchs8blt.com, will be replaced in the target file.
+ */
+
+$options['uri'] = 'http://local.jchs8blt.com';
